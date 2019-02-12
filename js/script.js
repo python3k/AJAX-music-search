fc4d3d346331580659a3512c2af1368c09527b0c
L <reneelsq@hotmail.com>
2019-02-12 16:49:21 +0100

    files updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,78 @@
-{   
+$(document).ready(function(){   
+    $.ajaxSetup({cache:false});
+    $("#myInput").keyup(function(){
+        $("#results").html();
+        $("#state").val();
+        let searchField = $("#myInput").val();
+        let expression = new RegExp(searchField, "i");
 
-    const createResultList = (results) => {
- 
-    }
+        let url="https://musicdemons.com/api/v1/artist";
+        let names=[];
+        
 
-    const search = value => {
+        fetch(url)
+        .then((res)=>res.json())
+        .then((data)=>{
+            for(let i=0; i<data.length; i++){
+                let name = data[i].name;
+                let id = data[i].id;
+                names.push(name);
+                console.log(data);
+               $.each(data,function(key,value){
+                  if(value.name.search(expression)!=-1){
+                  $("#results").append(`<li class="list-group-item"><a href="#${id}">${name}</a></li>`);
+                  }
+               });
+            };
+          
+           /*  for(let i=0; i<data.length; i++){
+               let name = data[i].name;
+               let id = data[i].id;
+               names.push(name);
+               if(name.includes(searchField)==true){
+                $("ul").append(`<li class="list-group-item"><a href="#${id}">${name}</a></li>`);  
+               } 
+           }  */
+         })
+        .catch((err)=>console.log(err));              
+
+
+     }); 
+
+
+    
+
+});  
+      
+         
+    
+     /* const createResultList = (results) => {
+         
+        $("input").focus(function(){
+            let searchField = $("input").val();
+            let url="https://musicdemons.com/api/v1/artist";
+            let names=[];
+
+            fetch(url)
+            .then((res)=>res.json())
+            .then((data)=>{
+               console.log(data);
+               for(let i=0; i<data.length; i++){
+                   let name = data[i].name;
+                   let id = data[i].id;
+                   names.push(name);
+                   if(name.includes(searchField)==true){
+                    $("ul").append(`<li class="list-group-item"><a href="#${id}">${name}</a></li>`);  
+                   } 
+               } 
+             })
+            .catch((err)=>console.log(err));              
+         }); 
+    };  
+
+
+
+      const search = value => {
         const url = ``;
 
     };
@@ -18,6 +86,24 @@
         document.querySelector(`.search`).addEventListener(`keyup`, handleKeyUpSearch);
     };
 
-    init();
+    init(); */
 
-}
+
+
+    // this does not work
+      /*  document.querySelector("#myInput").addEventListener("keyup",function(e){
+           const input = e.currentTarget;
+           const options = {
+               method:"POST",
+               headers:{
+                   "Content-type": "application/x-www-form-urlencoded"
+               },
+               body:name=`${input.value}`
+           };
+           fetch("https://musicdemons.com/api/v1/artist/autocomplete", options)
+           .then((res)=>res.json())
+           .then((data)=>{
+               console.log(data);
+           })
+           .catch((err)=>console.log(err));
+       }); */
