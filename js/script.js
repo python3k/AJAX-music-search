28c0680cda7db90d52583ba4c16beaab5b139009
ShuqinL <reneelsq@hotmail.com>
2019-02-13 22:45:14 +0100

    file updated

--- js/script.js
+++ js/script.js
@@ -1,74 +1,81 @@
 $(document).ready(function(){   
-    $.ajaxSetup({cache:false});
+   // var artistId;
+    //$.ajaxSetup({cache:false});
     $("#myInput").keyup(function(){
-        $("#results").html();
-        $("#state").val();
+      /*   $("#results").html();
+        $("#state").val(); */
         let searchField = $("#myInput").val();
         let expression = new RegExp(searchField, "i");
 
         let url="https://musicdemons.com/api/v1/artist";
         let names=[];
         
+        console.log('reset()');
+        $("#results").html('');
 
-        fetch(url)
+        fetch(url, {})
         .then((res)=>res.json())
         .then((data)=>{
+      
+            // reset
+            // add all relevant items (again)
             for(let i=0; i<data.length; i++){
                 let name = data[i].name;
                 let id = data[i].id;
-                names.push(name);
-                console.log(data);
-               $.each(data,function(key,value){
-                  if(value.name.search(expression)!=-1){
-                  $("#results").append(`<li class="list-group-item"><a href="#${id}">${name}</a></li>`);
-                  }
-               });
-            };
-          
-           /*  for(let i=0; i<data.length; i++){
-               let name = data[i].name;
-               let id = data[i].id;
-               names.push(name);
-               if(name.includes(searchField)==true){
-                $("ul").append(`<li class="list-group-item"><a href="#${id}">${name}</a></li>`);  
-               } 
-           }  */
+                
+                if(name.search(expression)!=-1){
+                    console.log(data[i]);
+                    // let li = document.createElement('li');
+                    // li.addClass('list-group-item');
+                    // let a = document.createElement('a');
+                    
+                    // li.append(a);
+                    // a.addEventListener(`click`, function(){
+                    //     console.log($("a").getAttribute("href"));
+                    //     console.log('id:' + artistId);   
+                    // });
+                    // $("#results").append(li);
+                   $("#results").append(
+                       `<li class="list-group-item">
+                            <a id="artist-${id}" href="#${id}">${name}</a>
+                        </li>`);
+                    
+                    //artistId = id;
+                }
+            }
+
+            //a beter way to replace the for loop
+            /*$.each(data,function(key,value){
+                if(value.name.search(expression)!=-1){
+                    console.log("found it" + value);
+                $("#results").append(`<li class="list-group-item"><a href="#${id}">${name}</a></li>`);
+                }
+             });*/
          })
         .catch((err)=>console.log(err));              
 
-
      }); 
 
-
-    
-
+ /*     document.querySelector(`#show`)
+        .addEventListener(`click`, function(){
+            //console.log($("a").getAttribute("href"));
+            console.log('id:' + document.location.hash);
+            
+        }); */
+        let artistId = document.location.hash;
+     $("a").click(function(){
+       
+         console.log('no reaction' + artistId);
+         //console.log($("a").getAttribute("href"));
+         //fetch("https://musicdemons.com/api/v1/artist")
+    });
+ 
 });  
       
          
     
-     /* const createResultList = (results) => {
-         
-        $("input").focus(function(){
-            let searchField = $("input").val();
-            let url="https://musicdemons.com/api/v1/artist";
-            let names=[];
-
-            fetch(url)
-            .then((res)=>res.json())
-            .then((data)=>{
-               console.log(data);
-               for(let i=0; i<data.length; i++){
-                   let name = data[i].name;
-                   let id = data[i].id;
-                   names.push(name);
-                   if(name.includes(searchField)==true){
-                    $("ul").append(`<li class="list-group-item"><a href="#${id}">${name}</a></li>`);  
-                   } 
-               } 
-             })
-            .catch((err)=>console.log(err));              
-         }); 
-    };  
+     /* const createResultList = (results) => {        
+         };  
 
 
 
@@ -100,7 +107,7 @@ $(document).ready(function(){
                },
                body:name=`${input.value}`
            };
-           fetch("https://musicdemons.com/api/v1/artist/autocomplete", options)
+           fetch("https://musicdemons.com/api/v1/artist", options)
            .then((res)=>res.json())
            .then((data)=>{
                console.log(data);
