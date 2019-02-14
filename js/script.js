e87d937af339dea0f510c89a2a8a61f44da0664b
L <reneelsq@hotmail.com>
2019-02-14 10:24:23 +0100

    video finished

--- js/script.js
+++ js/script.js
@@ -1,47 +1,49 @@
 $(document).ready(function(){   
-   // var artistId;
-    //$.ajaxSetup({cache:false});
+
     $("#myInput").keyup(function(){
-      /*   $("#results").html();
-        $("#state").val(); */
+     
         let searchField = $("#myInput").val();
         let expression = new RegExp(searchField, "i");
 
         let url="https://musicdemons.com/api/v1/artist";
-        let names=[];
-        
-        console.log('reset()');
+        //let names=[];     
         $("#results").html('');
 
-        fetch(url, {})
+        fetch(url)
         .then((res)=>res.json())
-        .then((data)=>{
-      
+        .then((data)=>{    
             // reset
-            // add all relevant items (again)
             for(let i=0; i<data.length; i++){
                 let name = data[i].name;
                 let id = data[i].id;
                 
                 if(name.search(expression)!=-1){
-                    console.log(data[i]);
-                    // let li = document.createElement('li');
-                    // li.addClass('list-group-item');
-                    // let a = document.createElement('a');
-                    
-                    // li.append(a);
-                    // a.addEventListener(`click`, function(){
-                    //     console.log($("a").getAttribute("href"));
-                    //     console.log('id:' + artistId);   
-                    // });
-                    // $("#results").append(li);
-                   $("#results").append(
+                   // console.log(data[i]);
+                    $("#results").append(
                        `<li class="list-group-item">
                             <a id="artist-${id}" href="#${id}">${name}</a>
                         </li>`);
-                    
-                    //artistId = id;
                 }
+
+                $("#artist-"+id).click(function(){       
+                  
+                     fetch("https://musicdemons.com/api/v1/artist/"+id+"/songs")
+                    .then((res)=>res.json())
+                    .then((data)=>{
+                         for(let i = 0; i<data.length; i++){
+                             let videoID = data[i].youtube_id;
+                             let videoURL= "https://www.youtube.com/embed/"+videoID;
+                             $(".songs").append(
+                                `<li class="list-group-item">
+                                    <iframe width="420" height="315" src=${videoURL}></iframe>
+                                 </li>`
+                             );
+                             
+                         }
+                        
+                    })
+                    .catch((err)=>console.log(err));
+                });
             }
 
             //a beter way to replace the for loop
@@ -53,22 +55,11 @@ $(document).ready(function(){
              });*/
          })
         .catch((err)=>console.log(err));              
-
      }); 
 
- /*     document.querySelector(`#show`)
-        .addEventListener(`click`, function(){
-            //console.log($("a").getAttribute("href"));
-            console.log('id:' + document.location.hash);
-            
-        }); */
-        let artistId = document.location.hash;
-     $("a").click(function(){
-       
-         console.log('no reaction' + artistId);
-         //console.log($("a").getAttribute("href"));
-         //fetch("https://musicdemons.com/api/v1/artist")
-    });
+
+
+
  
 });  
       
@@ -96,21 +87,3 @@ $(document).ready(function(){
     init(); */
 
 
-
-    // this does not work
-      /*  document.querySelector("#myInput").addEventListener("keyup",function(e){
-           const input = e.currentTarget;
-           const options = {
-               method:"POST",
-               headers:{
-                   "Content-type": "application/x-www-form-urlencoded"
-               },
-               body:name=`${input.value}`
-           };
-           fetch("https://musicdemons.com/api/v1/artist", options)
-           .then((res)=>res.json())
-           .then((data)=>{
-               console.log(data);
-           })
-           .catch((err)=>console.log(err));
-       }); */
